575b598c8ba0971b8e965664627691f35b207579
afsona571 <afsonaa59@gmail.com>
2024-11-27 17:57:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let arr = ['banan', 'shaftoli', 'olma', 'nok']
+console.log(arr);
+
+alert('array uzunkigi ' + arr.length)
+
+let con = confirm('biz arraydan bita meva aymoqchimiz ')
+if(con){
+    arr.pop()
+}
+
+
+
+console.log(arr);
